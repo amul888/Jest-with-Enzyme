c0fc6381be07308251a8db147ebed10c68a23fef
Amul Chandra <AmulC@emids.com>
2019-09-16 16:53:10 +0530

    start test for guessed words

--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -12,10 +12,10 @@ export const findByTestAttr = (wrapper, val) => {
     return wrapper.find(`[data-test="${val}"]`)
 }
 
-export const checkProps =(component, conformuingProps)=>{
+export const checkProps =(component, conformingProps)=>{
     const propError = CheckPropTypes(
         component.propTypes,
-        conformuingProps,
+        conformingProps,
         'prop',
         component.name)
         expect(propError).toBeUndefined();
