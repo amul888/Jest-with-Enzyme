6534c4e52f4d03e82180f3458e5010475de919cb
Amul Chandra <AmulC@emids.com>
2019-09-23 15:09:18 +0530

    complete Input component render tests for success=false

--- src/Input.js
+++ src/Input.js
@@ -4,15 +4,30 @@ import { connect } from "react-redux";
 
 class Input extends Component {
     render() {
+        const contents = this.props.success ? null : (
+            <form className="form-inline">
+                <input
+                    data-test="input-box"
+                    className="mb-2 mx-sm-3"
+                    placeholder="enter guess"
+                />
+                <button
+                    data-test="submit-button"
+                    className="btn btn-primary mb-2">
+                    type="submit"
+                    Submit
+                    </button>
+            </form>
+        )
         return (
-            <div >
-                <button/>
-                </div>
+            <div data-test="component-input">
+                {contents}
+            </div>
         )
     }
 }
 
-const mapStateToProps = (state) =>{
+const mapStateToProps = (state) => {
     return {}
 }
 export default connect(mapStateToProps)(Input)
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,38 +1,62 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import { findByTestArr, storeFactory } from "./test/testUtils";
+import {findByTestAttr, storeFactory } from "./test/testUtils";
 import Input from "./Input";
 
-const setup=(initialState={})=>{
+const setup = (initialState = {}) => {
     const store = storeFactory(initialState);
-    const wrapper = shallow(<Input store={store}/>).dive().dive();
+    const wrapper = shallow(<Input store={store} />).dive().dive();
     //console.log(wrapper.debug());
     return wrapper;
 }
 setup();
-describe("render",()=>{
-    describe("word has not been guessed",()=>{
-        test("renders componet without error",()=>{
+describe("render", () => {
+    describe("word has not been guessed", () => {
+        test("renders componet without error", () => {
 
         });
-        test("render input box",()=>{
+        test("render input box", () => {
 
         });
-        test("renders submit button ",()=>{
+        test("renders submit button ", () => {
 
         });
-    })
- 
-})
-describe("update state",()=>{
-    test("renders componet without error",()=>{
-
     });
-    test("render input box",()=>{
 
-    });
-    test("does not render submit button",()=>{
+    describe("word has been guessed", () => {
+        let wrapper;
+        beforeEach(() => {
+            const initialState = { success: false };
+            //wrapper = setup(initialState); // getting message : Unexpected key "success" found in preloadedState argument passed to createStore. Expected to find one of the known reducer keys instead: "sucessReducer". Unexpected keys will be ignored.
+            wrapper = setup();
 
+        })
+        test("renders componet without error", () => {
+            const componet =findByTestAttr(wrapper, "component-input");
+            expect(componet.length).toBe(1);
+        });
+        test("does not render input box", () => {
+            const inputBox =findByTestAttr(wrapper, "input-box");
+            expect(inputBox.length).toBe(1);
+        });
+        test("does not render submit button", () => {
+            const submitButton =findByTestAttr(wrapper, "submit-button");
+            expect(submitButton.length).toBe(1);
+        });
     });
+
+});
+
+
+describe("update state", () => {
+    // test("renders componet without error", () => {
+
+    // });
+    // test("render input box", () => {
+
+    // });
+    // test("does not render submit button", () => {
+
+    // });
 })
