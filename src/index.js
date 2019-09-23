cd0209f2ffe8828959f9de67f2d3fae0435b3f8d
Amul Chandra <AmulC@emids.com>
2019-09-23 12:40:59 +0530

    configure redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
 import App from "./App.js";
 import Congrats from "./Congrats";
+import store from "./configureStore";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById("root"));
 // ReactDOM.render(<Congrats />, document.getElementById("root"));
