a9329d373c195fd4afb3c3e2cd03295a45274f06
Amul Chandra <AmulC@emids.com>
2019-09-24 12:55:27 +0530

    axios tests done

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,4 +1,9 @@
+import moxios from "moxios";
+
 import { correctGuess, actionTypes } from "./";
+import { storeFactory } from "../test/testUtils";
+import { getSecretWord } from "./";
+
 
 describe('correctGuess',()=>{
     test('return an action with type `CORRECT_GUESS`',()=>{
@@ -6,3 +11,30 @@ describe('correctGuess',()=>{
         expect(action).toEqual({type: actionTypes.CORRECT_GUESS});
     });
 });
+
+
+describe("getSecretWord action creator",()=>{
+    beforeEach(()=>{
+        moxios.install();
+    });
+    afterEach(()=>{
+        moxios.uninstall     
+    });
+    test("adds response word to state",()=>{
+        const secretWord ="party";
+        const store = storeFactory();
+        moxios.wait(()=>{
+            const request = moxios.requests.mostRecent();
+            request.respondWith({
+                status:200,
+                response:secretWord
+            });
+        });
+        return store.dispatch(getSecretWord())
+        .then(()=>{
+            const newState = store.getState(); 
+            expect(newState.secretWord).toBe(secretWord)
+        })
+    });
+
+})
