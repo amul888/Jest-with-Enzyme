8a52a72ef1df893bd99f48bce1da997afa410331
Amul Chandra <AmulC@emids.com>
2019-09-26 12:23:04 +0530

    Connect app component and test

--- src/App.js
+++ src/App.js
@@ -1,50 +1,80 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import "./App.css";
 import ClickCounter from "./clickCounter";
 import GuessedWords from "./GuessedWords";
 import Congrats from "./Congrats";
 import Input from "./Input";
+import { getSecretWord } from "./actions";
+
+// click counter 1
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       counter: 0
+//     }
+//   }
+//   render() {
+//     const decrementValue = (this.state.counter < 0) ? 0 : this.state.counter - 1;
+//     const guessedWords = [
+//       { guessedWord: 'train', letterMatchCount: 3 },
+//       { guessedWord: 'agile', letterMatchCount: 1 },
+//       { guessedWord: 'party', letterMatchCount: 5 },
+//   ]
+//     return (
+//       <div data-test="component-app" className="container">
+//         <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+//         {this.state.counter < 0 && <h1 data-test="counter-display">The counter can not go below 0 </h1>}
+
+//         <button
+//           data-test="increment-button"
+//           onClick={() => this.setState({ counter: this.state.counter + 1 })}
+//         >Increment counter</button>
+//         <button
+//           data-test="dcrement-button"
+//           onClick={() => this.setState({ counter: decrementValue })}
+//         >Dcrement counter</button>
+//         <Congrats success = {true}/>
+//         <GuessedWords guessedWords={guessedWords}/>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0
-    }
-  }
-  render() {
-    const decrementValue = (this.state.counter < 0) ? 0 : this.state.counter - 1;
-    const guessedWords = [
-      { guessedWord: 'train', letterMatchCount: 3 },
-      { guessedWord: 'agile', letterMatchCount: 1 },
-      { guessedWord: 'party', letterMatchCount: 5 },
-  ]
-    return (
-      <div data-test="component-app" className="container">
-        <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
-        {this.state.counter < 0 && <h1 data-test="counter-display">The counter can not go below 0 </h1>}
-
-        <button
-          data-test="increment-button"
-          onClick={() => this.setState({ counter: this.state.counter + 1 })}
-        >Increment counter</button>
-        <button
-          data-test="dcrement-button"
-          onClick={() => this.setState({ counter: decrementValue })}
-        >Dcrement counter</button>
-        <Congrats success = {true}/>
-        <GuessedWords guessedWords={guessedWords}/>
-      </div>
-    );
-  }
-}
-
-export default App;
 
 {/* <div className="App">
 <h1> Hello, World! </h1>
 <ClickCounter/>
 </div> */}
+
+
+class App extends Component {
+  render() {
+    const guessedWords = [
+      { guessedWord: 'train', letterMatchCount: 3 },
+      { guessedWord: 'agile', letterMatchCount: 1 },
+      { guessedWord: 'party', letterMatchCount: 5 },
+    ]
+    return (
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={this.props.success} />
+        <Input/>
+        <GuessedWords guessedWords={this.props.guessedWords} />
+      </div>
+    )
+  }
+}
+const mapStateToProps =(state)=>{
+  const {success, guessedWords, secretWord} = state;
+  return {success, guessedWords, secretWord};
+}
+export default connect(mapStateToProps,{ getSecretWord })(App)
 
 
--- src/App.test.js
+++ src/App.test.js
@@ -3,72 +3,108 @@ import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 
 import App from "./App";
-import { exportAllDeclaration } from "@babel/types";
+import { storeFactory } from "./test/testUtils";
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-/**
- * 
- * Factory function to create a shallowWrapper for the App Component
- * @function setup
- * @param {object} props- Component props specific to this setup
- * @param {object} state= Initial satte fro setup
- * @return {shallawWrapper} 
- */
-
-const setup = (props = {}, state = null) => {
-    const wrapper = shallow(<App {...props} />);
-    if(state) wrapper.setState(state);
-    return wrapper; 
+const setup = (state={})=>{
+    const store = storeFactory(state);
+    const wrapper = shallow(<App store={store}/>).dive().dive();
+    return wrapper;
 }
 
-/**
- * 
- * return ShallowWrapper conating node(s) with the given data-test value
- * @param {ShallowWrapper} wrapper-Enyme shallow wrapper to search within
- * @param {string} val -Value of data-test attribute for search.
- * @return {ShallowWrapper}
- */
-const findByTestAttr = (wrapper, val) => {
-    return wrapper.find(`[data-test="${val}"]`)
-}
+describe("redux properties",()=>{
+    test("has access to `success` state",()=>{
+        const success = true;
+        const wrapper = setup({ success });
+        const sucessProp = wrapper.instance().props.success;
+        expect(sucessProp).toBe(success);
+    });
+    test("has access to `secretWord` state",()=>{
+        const secretWord ="party";
+        const wrapper = setup({ secretWord });
+        const secretWordProp = wrapper.instance().props.secretWord;
+        expect(secretWordProp).toBe(secretWord);
+    });
+    test("has access to `guessedWords` state",()=>{
+        const guessedWords =[{guessedWord:"train",letterMatchCount:3}];
+        const wrapper = setup({ guessedWords});
+        const guessedWordsProp = wrapper.instance().props.guessedWords;
+        expect(guessedWordsProp).toBe(guessedWords)
+    });
+    test("`getSecretWord` action creator is a function on the props",()=>{
+        const wrapper = setup();
+        const getSecretWordProp = wrapper.instance().props.getSecretWord;
+        expect(getSecretWordProp).toBeInstanceOf(Function);
+    });
+})
 
-test("renders without error", () => {
-    const wrapper = setup();
-    const appComponent = findByTestAttr(wrapper,'component-app');
-    expect(appComponent.length).toBe(1);
-});
+//previous test ======================
 
 
-test("renders increment button", () => {
-    const wrapper = setup();
-    const button = findByTestAttr(wrapper,'increment-button');
-    expect(button.length).toBe(1);
-});
+// Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test("renders counter display", () => {
-    const wrapper = setup();
-    const counterDispaly = findByTestAttr(wrapper,'counter-display');
-    expect(counterDispaly.length).toBe(1);
-});
+// /**
+//  * 
+//  * Factory function to create a shallowWrapper for the App Component
+//  * @function setup
+//  * @param {object} props- Component props specific to this setup
+//  * @param {object} state= Initial satte fro setup
+//  * @return {shallawWrapper} 
+//  */
 
-test("counter start at 0", () => {
-    const wrapper = setup();
-    const initialCounterState = wrapper.state('counter');
-    expect(initialCounterState).toBe(0);
-});
+// const setup = (props = {}, state = null) => {
+//     const wrapper = shallow(<App {...props} />);
+//     if(state) wrapper.setState(state);
+//     return wrapper; 
+// }
 
-test("clicking button increments counter display", () => {
-    const counter = 7;
-    const wrapper = setup(null,{counter});
+// /**
+//  * 
+//  * return ShallowWrapper conating node(s) with the given data-test value
+//  * @param {ShallowWrapper} wrapper-Enyme shallow wrapper to search within
+//  * @param {string} val -Value of data-test attribute for search.
+//  * @return {ShallowWrapper}
+//  */
+// const findByTestAttr = (wrapper, val) => {
+//     return wrapper.find(`[data-test="${val}"]`)
+// }
 
-    //find button and click
-    const button = findByTestAttr(wrapper,'increment-button');
-    button.simulate('click');
+// test("renders without error", () => {
+//     const wrapper = setup();
+//     const appComponent = findByTestAttr(wrapper,'component-app');
+//     expect(appComponent.length).toBe(1);
+// });
+
+
+// test("renders increment button", () => {
+//     const wrapper = setup();
+//     const button = findByTestAttr(wrapper,'increment-button');
+//     expect(button.length).toBe(1);
+// });
+
+// test("renders counter display", () => {
+//     const wrapper = setup();
+//     const counterDispaly = findByTestAttr(wrapper,'counter-display');
+//     expect(counterDispaly.length).toBe(1);
+// });
+
+// test("counter start at 0", () => {
+//     const wrapper = setup();
+//     const initialCounterState = wrapper.state('counter');
+//     expect(initialCounterState).toBe(0);
+// });
+
+// test("clicking button increments counter display", () => {
+//     const counter = 7;
+//     const wrapper = setup(null,{counter});
+
+//     //find button and click
+//     const button = findByTestAttr(wrapper,'increment-button');
+//     button.simulate('click');
     
-    //find display and test value
-    const counterDispaly = findByTestAttr(wrapper,'counter-display');
-    expect(counterDispaly.text()).toContain(counter +1)
-});
+//     //find display and test value
+//     const counterDispaly = findByTestAttr(wrapper,'counter-display');
+//     expect(counterDispaly.text()).toContain(counter +1)
+// });
 
 
